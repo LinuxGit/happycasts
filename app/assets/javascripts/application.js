74c3aaff0f98ee48e867e60e9b6ab9a5370ab7ca
Peter Wang <happypeter1983@gmail.com>
2012-11-08 22:57:51 +0800

    works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,9 @@
 //= require vendor/jquery.jplayer.min
 //= require bootstrap
 //= require editable_comment
+//= require jquery.atwho
 //= require_self
+
+var name_list = [ "peter", "billie"]
+$('textarea').atWho('@', {data: name_list});
 
